13b6230bee0addb79d2222cfeca59eee3739f67c
Cipz <matteo.cipone@gmail.com>
2021-10-26 23:51:20 +0200

    Initial Commit

---
+++ I_am_bored/I_am_boredApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_boredApp.swift
+//  I_am_bored
+//
+//  Created by Matteo Cipone on 26.10.21.
+//
+
+import SwiftUI
+
+@main
+struct I_am_boredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
